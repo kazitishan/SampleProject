1560cfe97d84ca56146108b4c9c65054efd54f8b
kazit <ktishan3135@bths.edu>
2022-09-21 21:50:13 -0400

    added favorite food

---
+++ src/SampleProject.java
@@ -0,0 +1,7 @@
+public class SampleProject {
+    public static void main(String[] args) {
+        System.out.print("Kazi");
+        System.out.print("Conjuring series");
+        System.out.print("Noodles");
+    }
+}
--- src/sample.java
+++
@@ -1,9 +0,0 @@
-import com.sun.tools.javac.Main;
-
-public class sample {
-    public static void main(String[] args) {
-        System.out.println("Kazi");
-        System.out.println("Conjuring Series");
-    }
-
-}
